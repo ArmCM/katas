1174783e0dd0f9b03b87496877f881eba509200f
ArmCM <ackurtcobain15@gmail.com>
2020-06-20 14:48:30 -0500

    refactoriza usando loop for

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -9,22 +9,17 @@ class PrimeFactors
      * 2 if true, then divisible by 2. if false, increase candidate and try again.
      * 3 repeat.
      *
-     * @param  $number
+     * @param  int  $number
      * @return array
      */
     public function generate($number)
     {
         $factors = [];
-        $divisor = 2;
 
-        while ($number > 1) {
-            while ($number % $divisor === 0) {
-                $factors[] = $divisor;
-
-                $number = $number / $divisor;
+        for ($divisor = 2; $number > 1; $divisor++) { // div(3)   num(3) true   div++(3)
+            for (;$number % $divisor === 0; $number = $number / $divisor) {  //  3 % 3 true,   6 / 2 = 3 num
+                $factors[] = $divisor; // [2, 2, 2, 3]
             }
-
-            $divisor++;
         }
 
         return $factors;
--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -18,30 +18,6 @@ class PrimeFactorsTest extends TestCase
         $this->assertEquals($expected, $factors->generate($number));
     }
 
-    /** @test */
-    public function it_generates_prime_factors_for_2()
-    {
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([2], $factors->generate(2));
-    }
-
-    /** @test */
-    public function it_generates_prime_factors_for_3()
-    {
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([3], $factors->generate(3));
-    }
-
-    /** @test */
-    public function it_generates_prime_factors_for_4()
-    {
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([2, 2], $factors->generate(4));
-    }
-
     public function factors()
     {
         return [
@@ -52,6 +28,7 @@ class PrimeFactorsTest extends TestCase
             [5, [5]],
             [6, [2, 3]],
             [8, [2, 2, 2]],
+            [24, [2,2,2,3]],
             [100, [2, 2, 5, 5]],
         ];
     }
