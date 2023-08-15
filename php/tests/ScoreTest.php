ee03ab3c0e3026ce65990819e7a72082a762e325
Armando Monreal <acalderon.root@gmail.com>
2023-08-15 12:47:33 -0600

    Rename class to Score and add fail test

--- php/tests/ScoreTest.php
+++ php/tests/ScoreTest.php
@@ -2,14 +2,15 @@
 
 use PHPUnit\Framework\TestCase;
 
-class CalculatePointsTest extends TestCase
+class ScoreTest extends TestCase
 {
     /** @test */
     public function it_add_one_point_if_bob_win_a_turn()
     {
-        $alice = [2];
-        $bob = [5];
-        $points = new CalculatePoints($alice, $bob);
+        $alicePoints = [2];
+        $bobPoints = [5];
+
+        $points = Score::sum($alicePoints, $bobPoints);
 
         $this->assertEquals([0, 1], $points);
     }
