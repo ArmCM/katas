c19fae7617a1cd6613d521727e55333d39f45177
Armando Monreal <acalderon.root@gmail.com>
2023-08-16 00:27:08 -0600

    Create a failed test to handle Exception when words hasn't vowels

--- php/tests/CountVowelsTest.php
+++ php/tests/CountVowelsTest.php
@@ -11,4 +11,12 @@ class CountVowelsTest extends TestCase
         $this->assertEquals(5, (new Vowels('abracadabra'))->count());
         $this->assertEquals(3, (new Vowels('example'))->count());
     }
+
+    /** @test */
+    public function words_without_vowels_not_allowed()
+    {
+        $this->expectException(\Exception::class);
+
+        (new Vowels('gym'))->count();
+    }
 }
